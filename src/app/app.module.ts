4e9212cf561328be3321c357ff4877f2a660f6c7
Shashank <sunnyrocks1122@gmail.com>
2020-07-29 12:15:25 +0530

    Header/Footer Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { HelloComponent } from './hello.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MaterialModule } from '../material';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, MatIconModule, MaterialModule ],
-  declarations: [ AppComponent, HelloComponent, HeaderComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
